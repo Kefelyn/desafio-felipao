41042d843729aacbd8ca8649c9efc288d16c3727
Kefelyn <kefelyn.vitoria4@gmail.com>
2024-10-14 21:49:42 -0300

    Desafio finalizado

--- index.js
+++ index.js
@@ -3,34 +3,27 @@ let Exp = 40000
 let nivel = ""
 
 if(Exp <1000){
-    nivel = "Ferro" 
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+    nivel = "Ferro"}
 
 else if(Exp >1001 && Exp <2000){
-    nivel = "Bronze" 
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+    nivel = "Bronze"}
         
 else if(Exp >2001 && Exp <5000){
-    nivel = "Prata" 
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+    nivel = "Prata"}
 
 else if(Exp >5001 && Exp <7000){
-    nivel = "Ouro" 
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+    nivel = "Ouro"}
         
 else if(Exp >7001 && Exp <8000){
-    nivel = "Platina" 
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+    nivel = "Platina"}
 
 else if(Exp >8001 && Exp <9000){
-    nivel = "Ascendente" 
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+    nivel = "Ascendente"}
 
 else if(Exp >9001 && Exp <10000){
-    nivel = "Imortal" 
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+    nivel = "Imortal"}
 
 else if(Exp >= 10001){
-    nivel = "Radiante" 
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+    nivel = "Radiante"}
   
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
