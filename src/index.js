81a399070c2305eccc2ad4fd38394a61cdb76eac
Kefelyn <kefelyn.vitoria4@gmail.com>
2024-10-14 21:22:19 -0300

    desafio

---
+++ src/index.js
@@ -0,0 +1,37 @@
+let nomeHeroi = "Kesty"
+let Exp = 40.000
+let nivel = ""
+
+if(Exp <1.000){
+    
+    nivel = "Ferro" 
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+
+else if(Exp >1.001 && Exp <2.000){
+    nivel = "Bronze" 
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+        
+else if(Exp >2.001 && Exp <5.000){
+    nivel = "Prata" 
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+
+else if(Exp >5.001 && Exp <7.000){
+    nivel = "Ouro" 
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+        
+else if(Exp >7.001 && Exp <8.000){
+    nivel = "Platina" 
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+
+else if(Exp >8.001 && Exp <9.000){
+    nivel = "Ascendente" 
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+
+else if(Exp >9.001 && Exp <10.000){
+    nivel = "Imortal" 
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+
+else if(Exp >10.001 && Exp <10.001){
+    nivel = "Radiante" 
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)}
+  
