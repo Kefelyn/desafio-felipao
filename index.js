ad88c68a664ee0af1888b0c58eae77c61db1e2e1
Kefelyn <kefelyn.vitoria4@gmail.com>
2024-10-14 22:31:03 -0300

    Desafio finalizado

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 let nomeHeroi = "Kesty"
-let Exp = 40000
+let exp = 40000
 let nivel = ""
 
-if(Exp <1000){
+if(exp <1000){
     nivel = "Ferro"}
 
-else if(Exp >1001 && Exp <2000){
+else if(exp >1001 && exp <2000){
     nivel = "Bronze"}
         
-else if(Exp >2001 && Exp <5000){
+else if(exp >2001 && exp <5000){
     nivel = "Prata"}
 
-else if(Exp >5001 && Exp <7000){
+else if(exp >5001 && exp <7000){
     nivel = "Ouro"}
         
-else if(Exp >7001 && Exp <8000){
+else if(exp >7001 && exp <8000){
     nivel = "Platina"}
 
-else if(Exp >8001 && Exp <9000){
+else if(exp >8001 && exp <9000){
     nivel = "Ascendente"}
 
-else if(Exp >9001 && Exp <10000){
+else if(exp >9001 && exp <10000){
     nivel = "Imortal"}
 
-else if(Exp >= 10001){
+else if(exp >= 10001){
     nivel = "Radiante"}
   
 console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
